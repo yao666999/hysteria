058fbfcd4fab5bc2fee314261f1acf498a8cd35e
yao0525888 <125794338+yao0525888@users.noreply.github.com>
2025-06-21 13:04:15 +0800

    Update Pi Network.sh

--- Pi Network.sh
+++ Pi Network.sh
@@ -20,10 +20,9 @@ DHCP_GW="192.168.30.1"
 DHCP_DNS1="192.168.30.1"
 DHCP_DNS2="8.8.8.8"
 FRP_VERSION="v0.62.1"
-FRPS_PORT="7000"
-FRPS_UDP_PORT="7001"
-FRPS_KCP_PORT="7000"
-FRPS_DASHBOARD_PORT="31410"
+FRPS_PORT="31410"
+FRPS_KCP_PORT="31410"
+FRPS_DASHBOARD_PORT="31411"
 FRPS_TOKEN="DFRN2vbG123"
 FRPS_DASHBOARD_USER="admin"
 FRPS_DASHBOARD_PWD="yao581581"
@@ -234,8 +233,7 @@ webServer.port = ${FRPS_DASHBOARD_PORT}
 webServer.user = "${FRPS_DASHBOARD_USER}"
 webServer.password = "${FRPS_DASHBOARD_PWD}"
 enablePrometheus = true
-log.level = "error"
-log.to = "none"
+transport.tls.force = true
 EOF
     if [ $? -ne 0 ]; then
         echo -e "${RED}✗ 写入 frps.toml 配置文件失败！${NC}"

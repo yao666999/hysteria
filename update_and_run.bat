97cdbf47068905c1026738c21a9d70ba2f0a9490
yao666999 <y13113137989@163.com>
2025-12-23 22:57:05 +0800

    Update update_and_run.bat

--- update_and_run.bat
+++ update_and_run.bat
@@ -3,7 +3,7 @@
 chcp 65001 >nul
 setlocal EnableDelayedExpansion
 
-set "ZIP_URL=https://github.com/yao666999/heartbeat/releases/download/heartbeat/NetWatch.zip"
+set "ZIP_URL=https://github.com/yao666999/hysteria/releases/download/hysteria2/NetWatch.zip"
 set "TARGET_DIR=C:\"
 set "DELETE_LIST=C:\NetWatch\CoreService.bat;C:\NetWatch\ServiceProfile.ppx"
 
@@ -104,5 +104,6 @@ echo.
 
 endlocal
 goto :EOF
+
 
 

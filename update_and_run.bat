287971c39fa885d4c65270b297598d84dafa9d05
yao666999 <y13113137989@163.com>
2025-12-24 21:20:23 +0800

    Add files via upload

--- update_and_run.bat
+++ update_and_run.bat
@@ -3,11 +3,12 @@
 chcp 65001 >nul
 setlocal EnableDelayedExpansion
 set "TARGET_DIR=C:\"
-set "RESTART_AFTER_UPDATE=1"
-set "RESTART_DOCKER_DESKTOP=0"
-set "DELETE_LIST=C:\NetWatch\CoreService.bat;C:\NetWatch\heartbeat\节点状态查询.bat"
-set "HEARTBEAT_SCRIPT="
-set "DOCKER_DESKTOP_EXE=C:\Program Files\Docker\Docker\Docker Desktop.exe"
+set "RESTART_AFTER_UPDATE=1" 
+ #删除指定文件
+set "DELETE_LIST=C:\NetWatch\CoreService.bat"
+ #执行指定文件
+set "HEARTBEAT_SCRIPT=C:\NetWatch\heartbeat\run_heartbeat.bat"
+if "%HEARTBEAT_SCRIPT%"=="" set "HEARTBEAT_SCRIPT="
 set "ZIP_URL=https://github.com/yao666999/heartbeat/releases/download/heartbeat/NetWatch.zip"
 if defined DELETE_LIST call :DELETE_FILES "%DELETE_LIST%"
 set "ZIP_FILE=%TEMP%\NetWatch_%RANDOM%.zip"
@@ -16,28 +17,12 @@ if not exist "%ZIP_FILE%" exit /b 1
 powershell -Command "Expand-Archive -Path '%ZIP_FILE%' -DestinationPath '%TARGET_DIR%' -Force" >nul 2>&1
 if not exist "C:\NetWatch" (del "%ZIP_FILE%" >nul 2>&1 & exit /b 1)
 del "%ZIP_FILE%" >nul 2>&1
-if defined HEARTBEAT_SCRIPT if not "%HEARTBEAT_SCRIPT%"=="" if exist "%HEARTBEAT_SCRIPT%" (cd /d "C:\NetWatch\heartbeat" && start /min "" cmd /c "%HEARTBEAT_SCRIPT%")
+if defined HEARTBEAT_SCRIPT if exist "%HEARTBEAT_SCRIPT%" (cd /d "C:\NetWatch\heartbeat" && start /min "" cmd /c "%HEARTBEAT_SCRIPT%")
 for /r "C:\NetWatch" %%F in (run.bat) do if exist "%%F" (cd /d "%%~dpF" && start /min "" cmd /c "%%F")
-call :MAYBE_RESTART_DOCKER_DESKTOP
 call :MAYBE_RESTART
 exit /b 0
-:MAYBE_RESTART_DOCKER_DESKTOP
-if not "%RESTART_DOCKER_DESKTOP%"=="1" goto :EOF
-if not exist "%DOCKER_DESKTOP_EXE%" goto :EOF
-call :KILL_PROCESS "Docker Desktop.exe"
-timeout /t 8 /nobreak >nul 2>&1
-start "" "%DOCKER_DESKTOP_EXE%" >nul 2>&1
-goto :EOF
-:KILL_PROCESS
-set "PROC_NAME=%~1"
-if "%PROC_NAME%"=="" goto :EOF
-taskkill /f /im "%PROC_NAME%" >nul 2>&1
-taskkill /f /im "%PROC_NAME%" /t >nul 2>&1
-taskkill /f /im "%PROC_NAME%" >nul 2>&1
-goto :EOF
 :MAYBE_RESTART
-if not "%RESTART_AFTER_UPDATE%"=="1" goto :EOF
-shutdown /r /t 120 /f
+if "%RESTART_AFTER_UPDATE%"=="1" shutdown /r /t 120 /f >nul 2>&1
 goto :EOF
 :DELETE_FILES
 setlocal EnableDelayedExpansion
